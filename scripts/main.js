8ff904450eac8142f931148c37c7fdbcd554a6be
jash2110 <jashvadgama2110@gmail.com>
2025-06-21 11:19:19 +0530

    responsive

--- scripts/main.js
+++ scripts/main.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 const inputBox = document.getElementById("input-box");
 
 const listContainer = document.getElementById("list-container");
@@ -45,3 +46,57 @@ function showTask(){
     }
 }
 showTask();
+=======
+const inputBox = document.getElementById("input-box");
+
+const listContainer = document.getElementById("list-container");
+
+
+inputBox.addEventListener("keydown", function(event){
+    if(event.key === "Enter"){
+        addTask();
+    }
+});
+
+function addTask(){
+    if(inputBox.value === ''){
+        alert("You Must write something!");
+    }
+    else{
+        let li = document.createElement("li");
+        li.innerHTML = inputBox.value;
+        listContainer.appendChild(li);
+
+        let span = document.createElement("span");
+        span.innerHTML = "\u00d7";
+        li.appendChild(span);
+    }
+    inputBox.value = "";
+    saveData();
+}
+
+listContainer.addEventListener("click", function(e){
+    if(e.target.tagName === "LI"){
+        e.target.classList.toggle("checked");
+        saveData();
+    }
+    else if(e.target.tagName === "SPAN"){
+        e.target.parentElement.remove();
+        saveData();
+    }
+}, false);
+
+function saveData(){
+    localStorage.setItem("data", listContainer.innerHTML);
+}
+
+function showTask(){
+    let data = localStorage.getItem("data");
+    if(data){
+        listContainer.innerHTML = data;
+    } else {
+        listContainer.innerHTML = "";
+    }
+}
+showTask();
+>>>>>>> 921b507 (responsive)
